7fef93281dcef315efeca72b8411026c65f8a691
soullcake <soullcakezz@gmail.com>
2023-06-25 14:38:53 -0300

    feat
    registro e login funcioando

--- pi/register.php
+++ pi/register.php
@@ -4,15 +4,14 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 if ($method == 'POST') {
     if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
-    
         $username = $_POST['username'];
         $email = $_POST['email'];
         $password = $_POST['password'];
     
         $model = new User(connection());    
-        $data = $model->find($email);
+        $data = $model->save($username, $email, $password);
     
-        if ($data && password_verify($password, $data['password'])) {
+        if ($data) {            
             $_SESSION['username'] = $username;
             header('Location: /');
         }
