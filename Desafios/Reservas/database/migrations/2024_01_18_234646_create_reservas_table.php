cfd0405965b611fbe900d408e285e3146a8e9773
soullcake <soullcakezz@gmail.com>
2024-01-18 20:51:52 -0300

    reservas: migrations

---
+++ Desafios/Reservas/database/migrations/2024_01_18_234646_create_reservas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sala_id');
+            $table->unsignedBigInteger('usuario_id');
+            $table->datetime('data_inicio');
+            $table->datetime('data_fim');
+            $table->text('observacoes')->nullable();
+            $table->timestamps();
+
+            // Chaves estrangeiras
+            $table->foreign('sala_id')->references('id')->on('salas');
+            $table->foreign('usuario_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservas');
+    }
+};
