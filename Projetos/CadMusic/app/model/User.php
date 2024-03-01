e59f7a128236ef110e911e6edb8db7264edc4cb1
soullcake <soullcakezz@gmail.com>
2024-03-01 16:51:46 -0300

    Adicionando classe User e e funcao save
    a funcao save ira persistir o usuario no banco de dados

---
+++ Projetos/CadMusic/app/model/User.php
@@ -0,0 +1,21 @@
+<?php
+class User {
+
+protected $conn;
+public function __construct(SQLite3 $connection) {
+    $this->conn = $connection; 
+} 
+
+public function save (string $username, string $passwd) : SQLite3Result | bool{
+    $query = "INSERT INTO users ('username', 'passwd') "
+        . "values(:username,:passwd)";
+
+    $sttm = $this->conn->prepare($query);
+
+    $sttm->bindValue(":username", $username);
+    $sttm->bindValue(":passwd", password_hash($passwd, PASSWORD_ARGON2I));
+    $result = $sttm->execute();
+    return $result;
+}
+
+}
